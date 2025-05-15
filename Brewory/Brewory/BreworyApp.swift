ca3df0ac80fd4c83d12ecd391838cab222754094
NamJunLee <dlskawns96@gmail.com>
2025-05-15 23:27:09 +0900

    Created Project

---
+++ Brewory/Brewory/BreworyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreworyApp.swift
+//  Brewory
+//
+//  Created by Nam Jun Lee on 5/15/25.
+//
+
+import SwiftUI
+
+@main
+struct BreworyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
